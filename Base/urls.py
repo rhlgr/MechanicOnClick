2b81507c86ed475ba6aa44eef94d3a47e2fac741
Avim <avidubey2000@gmail.com>
2022-12-11 03:01:00 +0530

    About page added with url

--- Base/urls.py
+++ Base/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home , about , services
 urlpatterns = [
     path('', home ,name='home_page'),
+    path('about/' , about, name='about_page'),
+    path('service/' , services, name='service_page'),
 ]
--- Base/views.py
+++ Base/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'Base/home.html')
+
+def about(request):
+    return render(request,'Base/aboutus.html')
+
+def services(request):
+    return render(request,'Base/service_page.html')
