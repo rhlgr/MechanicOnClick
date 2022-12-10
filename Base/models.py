eac7bc97cb285228bacb63975e4c454c45c56999
Avim <avidubey2000@gmail.com>
2022-12-11 04:06:13 +0530

    Added On delete

--- Base/models.py
+++ Base/models.py
@@ -4,6 +4,6 @@ from ERP.models import Service
 class Booking(models.Model):
     name = models.CharField(max_length=50, null=True , blank=True)
     email = models.EmailField(null=True , blank=True)
-    services = models.ForeignKey(Service,null=True , blank=True )
+    services = models.ForeignKey(Service,null=True , blank=True , on_delete=models.SET_NULL)
     date = models.DateField(null=True , blank=True)
     content = models.CharField(max_length=200, null=True , blank=True)
