03f65a03df6405057fe729a9ea409662549dc1a2
Avim <avidubey2000@gmail.com>
2022-12-11 03:34:51 +0530

    Added booking url

--- Base/urls.py
+++ Base/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import home , about , services
+from .views import home , about , services , booking
 urlpatterns = [
     path('', home ,name='home_page'),
     path('about/' , about, name='about_page'),
     path('service/' , services, name='service_page'),
+    path('booking/' , booking, name='booking_page'),
+    
 ]
