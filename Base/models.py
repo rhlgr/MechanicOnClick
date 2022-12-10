79991e426cc18409fa05febfa9d5b22ba249ed20
Avim <avidubey2000@gmail.com>
2022-12-11 04:41:16 +0530

    Added Booking Form to home page

--- Base/models.py
+++ Base/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from ERP.models import Service
+from ERP.models import ProvidedService
 # Create your models here.
 class Booking(models.Model):
     name = models.CharField(max_length=50, null=True , blank=True)
     email = models.EmailField(null=True , blank=True)
-    services = models.ForeignKey(Service,null=True , blank=True , on_delete=models.SET_NULL)
+    ProvidedServices = models.ForeignKey(ProvidedService,null=True , blank=True , on_delete=models.SET_NULL)
     date = models.DateField(null=True , blank=True)
     content = models.CharField(max_length=200, null=True , blank=True)
--- Base/views.py
+++ Base/views.py
@@ -1,11 +1,24 @@
-from django.shortcuts import render
-
+from django.shortcuts import render , redirect
+from .forms import BookingForm
 # Create your views here.
 def home(request):
     if request.method == 'POST':
-        print('here')
-        print(request.POST.values())
-    return render(request,'Base/home.html')
+        print('In home post method')
+        form = BookingForm(request.POST)
+        if form.is_valid:
+            print('The form is Valid')
+            try :
+                form.save()
+                
+                return redirect('about_page')
+            except :
+                #messages.error(request, 'Invalid Credentials')
+                return redirect('booking_page')
+        else :
+             print('The form is Not Valid')
+    context ={}
+    context['form']= BookingForm()
+    return render(request,'Base/home.html',context=context)
 
 def about(request):
     return render(request,'Base/aboutus.html')
