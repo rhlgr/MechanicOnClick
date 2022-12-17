cdb6f0267754d058384e599d87642d2fef0c1abb
Avim <avidubey2000@gmail.com>
2022-12-18 03:28:37 +0530

    Added Fields to Employee Model

--- account/models.py
+++ account/models.py
@@ -10,11 +10,14 @@ class Customer(models.Model):
         return self.user.first_name
 
 class Employee(models.Model):
-    emp_img = models.ImageField(upload_to='static/empimg', default= 'static/empimg/AviDP.jpeg')
+    emp_img = models.ImageField(upload_to='static/empimg', default= 'media/empimg/AviDP.jpeg')
     user = models.OneToOneField(User , on_delete=models.CASCADE)
     center = models.ForeignKey(Center , on_delete= models.SET_NULL , blank=True , null= True)
     store = models.CharField(max_length=100)
     job_role = models.ForeignKey( JobRole, on_delete= models.SET_NULL, blank=True , null= True)
-    
+    on_display = models.BooleanField(default=False)
+    facbook_link = models.CharField(max_length=100, blank=True , null=True)
+    twitter_link = models.CharField(max_length=100, blank=True , null=True)
+    insta_link = models.CharField(max_length=100, blank=True , null=True)
     def __str__(self) -> str:
         return self.user.first_name + " "+ self.user.last_name
