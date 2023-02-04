5fb51cd476ccc4acf2ae55afe828c1c45e3d1a61
Avim <avidubey2000@gmail.com>
2023-02-04 22:38:24 +0530

    Added Brand and Model classes

--- Base/models.py
+++ Base/models.py
@@ -2,14 +2,25 @@ from django.db import models
 from ERP.models import ProvidedService
 from account.models import Employee
 # Create your models here.
-class Booking(models.Model):
-    name = models.CharField(max_length=50, null=True , blank=True)
-    email = models.EmailField(null=True , blank=True)
-    phone = models.CharField(max_length=50, null=True , blank=True)
-    #date = models.DateField(null=True , blank=True)
-    issue = models.CharField(max_length=200, null=True , blank=True)
-    vehical = models.CharField(max_length=50, null=True , blank=True)
+class Brand(models.Model):
+    logo = models.ImageField(upload_to='/brands/logos')
+    name = models.TextField(max_length=50)
+    has_two_wheeler = models.BooleanField(default=True)
+    has_four_wheeler = models.BooleanField(default=True)
+class VechicalModel(models.Model):
+    class FuleType(models.TextChoices):
+        PETROL = 'PETROL' , 'PETROL'
+        DIESEL = 'DIESEL' , 'DIESEL'
+        CNG = 'CNG' , 'CNG'
+    fule_type = models.TextField(max_length=50 , choices=FuleType.choices)
+    brand = models.ForeignKey(Brand , on_delete=models.CASCADE)
+    name = models.TextField(max_length=50)
 
+class Booking(models.Model):
+
+    phone = models.CharField(max_length=50, null=True , blank=True)
+    issue = models.CharField(max_length=200, null=True , blank=True)
+    
     def __str__(self) -> str:
         return self.name 
 
